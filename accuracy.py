e492716a316f0a57c51516a7c3e75aabee7ba24c
Jayasurya <jayasurya217@gmail.com>
2021-12-27 12:14:14 +0530

    Add files via upload
    Over a hundred thousand people are dying in road accidents in India every year among those 30% of accidents take place due to roads and buildings which are constructed without following any government regulations. To minimize road accidents we need to identify the buildings which are illegally constructed (without following any government regulations) on the roadside. The aim of this paper is divided into two parts. Firstly, segmenting the roads and buildings from the satellite image and secondly, finding out the illegal construction of roads and buildings by matching the current image with the timed image which is authorized by the government authorities. In order to carry out the objective on a larger scale, an innovative method is evolved which combines a semantic segmentation neural network with residual learning and U-net. In addition, it is proposed a method for road and building extraction from a satellite image. The convolution neural network is built using the U-net architecture. The two 
    main advantages of this network are (1) U-net is symmetric and (2) the skip connections between the down-sampling path and the up-sampling apply a concatenation operation instead of an addition operation when compared to standard Fully Conventional Neural network (FCN)-8. In this application, we used the Massachusetts Benchmark dataset for roads and building extraction. Essentially, in this research, a comparison was carried out with other methods recently available (RESNET, SEGNET), this method greater advantage in providing results even with not fully connected layers. Moreover, this network was found to be equally good in terms of computational time.
    Key terms: Road extraction, Building extraction, Convolutional Neural Network, U-Net, Convolutional 
    layer, Max pooling layer, Epoch.

---
+++ accuracy.py
@@ -0,0 +1,80 @@
+import csv
+import gdal
+import numpy as np
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import multilabel_confusion_matrix
+from tensorflow.keras.models import load_model
+import argparse
+from models import resunet_model, unet_model, segnet_model
+
+
+def accuracy(args):
+    if args.model == "unet":
+        model = unet_model.UNet(args)
+        model.load_weights(args.weights)
+   
+    else:
+        print("Enter the proper name dood....")
+    # print(model)
+    paths_file = args.csv_paths
+    test_image_paths = []
+    test_label_paths = []
+    with open(paths_file, 'r', newline='\n') as csvfile:
+        plots = csv.reader(csvfile, delimiter=',')
+        for row in plots:
+            test_image_paths.append(row[0])
+            test_label_paths.append(row[1])
+    y = []
+    y_pred = []
+    for i in range(len(test_image_paths)):
+        image = gdal.Open(test_image_paths[i]) #open the image folder test
+        image_array = np.array(image.ReadAsArray()) / 255
+        image_array = image_array.transpose(1, 2, 0)
+        label = gdal.Open(test_label_paths[i])
+        label_array = np.array(label.ReadAsArray())
+        label_array = np.expand_dims(label_array, axis=-1)
+        fm = np.expand_dims(image_array, axis=0)
+        result_array = model.predict(fm)
+        result_array = np.argmax(result_array[0], axis=2)
+        result_array = np.squeeze(result_array)
+        y.append(np.around(label_array))
+        y_pred.append(result_array)
+        print("Predicted " + str(i + 1) + " Images")
+    # print(len(np.array(y).flatten()), len(np.array(y_pred).flatten()))
+    print("\n")
+    cm = confusion_matrix(np.array(y).flatten(), np.array(y_pred).flatten())
+    cm_multi = multilabel_confusion_matrix(np.array(y).flatten(), np.array(y_pred).flatten())
+    print("Confusion Matrix " + "\n")
+    print(cm, "\n")
+    accuracy = np.trace(cm/np.sum(cm))
+    print("Overal Accuracy: ", round(accuracy, 3), "\n")
+
+    mean_iou = 0
+    mean_f1 = 0
+    for j in range(len(cm_multi)):
+        print("Class: " + str(j))
+        iou = cm_multi[j][1][1] / (cm_multi[j][1][1] + cm_multi[j][0][1] + cm_multi[j][1][0])
+        f1 = (2 * cm_multi[j][1][1]) / (2 * cm_multi[j][1][1] + cm_multi[j][0][1] + cm_multi[j][1][0])
+        precision = (cm_multi[j][1][1] / (cm_multi[j][1][1] + cm_multi[j][0][1]))
+        recall = (cm_multi[j][1][1] / (cm_multi[j][1][1] + cm_multi[j][1][0]))
+        mean_iou  += iou
+        mean_f1 += f1
+        print("IoU Score: ", round(iou, 3))
+        print("F1-Measure: ", round(f1, 3))
+        print("Precision: ", round(precision, 3))
+        print("Recall: ", round(recall, 3), "\n")
+        
+    print("Mean IoU Score: ", round(mean_iou/len(cm_multi), 3))
+    print("Mean F1-Measure: ", round(mean_f1/len(cm_multi), 3))
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model", type=str, help="Name of the model (from unet, resunet, or segnet)")
+    parser.add_argument("--input_shape", nargs='+', type=int, help="Input shape of the model (rows, columns, channels)")
+    parser.add_argument("--weights", type=str, help="Name and path of the trained model")
+    parser.add_argument("--csv_paths", type=str, help="CSV file with image and label paths")
+    parser.add_argument("--num_classes", type=int, help="Number of classes")
+    args = parser.parse_args()
+    accuracy(args)
---
+++ rasterize.py
@@ -0,0 +1,107 @@
+import ogr
+import osr
+import gdal
+import numpy as np
+import os, sys
+import glob
+import argparse
+
+
+def rasterize(args):
+    driver = ogr.GetDriverByName('ESRI Shapefile')
+    raster_layer = gdal.Open(args.raster)
+    vector_layer = driver.Open(args.vector, 0)
+
+    # (1)Check Projection of vector and raster datasets
+    ds = vector_layer.GetLayer().GetSpatialRef()
+    vector_epsg = ds.GetAttrValue('AUTHORITY', 1)
+    proj = osr.SpatialReference(wkt=raster_layer.GetProjection())
+    raster_epsg = proj.GetAttrValue('AUTHORITY', 1)
+    if vector_epsg != raster_epsg:
+        print("The projections of Vector and Raster files are not same")
+        sys.exit()
+
+    # (2) Creating the destination raster data source
+    output_file = args.output_file
+    ref_image = gdal.Open(args.raster)
+    gt = ref_image.GetGeoTransform()
+    proj = ref_image.GetProjection()
+    [cols, rows] = np.array(ref_image.GetRasterBand(1).ReadAsArray()).shape
+    target_ds = gdal.GetDriverByName('GTiff').Create(output_file, rows, cols, 1, gdal.GDT_Byte)
+    target_ds.SetGeoTransform(gt)
+    target_ds.SetProjection(proj)
+    target_ds.GetRasterBand(1)
+
+    v_layer = vector_layer.GetLayer()
+    featureTest = v_layer.GetNextFeature()
+    geom = featureTest.GetGeometryRef()
+    print(geom.GetGeometryType(), ogr.wkbLineString)
+
+    # (3) Creating polygon feature from line feature with buffer
+    if geom.GetGeometryType() == ogr.wkbLineString:
+
+        # Buffering
+        file = '../data/buffer.shp'
+        if os.path.exists(file):
+            driver.DeleteDataSource(file)
+        ds = driver.CreateDataSource(file)
+        if ds is None:
+            print('Could not create file')
+            sys.exit(1)
+
+        geosr = osr.SpatialReference()
+        geosr.ImportFromEPSG(int(vector_epsg))
+        lyrBuffer = ds.CreateLayer('buffer', geom_type=ogr.wkbPolygon, srs=geosr)
+        featureDefn = lyrBuffer.GetLayerDefn()
+
+        while featureTest:
+            geomTest = featureTest.GetGeometryRef()
+            # geomTest.Transform(geosr2utmsr)
+            if args.buffer_atr:
+                try:
+                    buffer_width = (float(featureTest.GetField(args.buffer_atr)) * args.buffer)
+                except ValueError:
+                    print("The type of attribute value should be integer or float")
+                    sys.exit()
+                except KeyError:
+                    print("Attribute does not exist in shapefile attributes")
+                    sys.exit()
+            else:
+                buffer_width = args.buffer
+            geomBuffer = geomTest.Buffer(buffer_width)
+            outFeature = ogr.Feature(featureDefn)
+            outFeature.SetGeometry(geomBuffer)
+            lyrBuffer.CreateFeature(outFeature)
+            outFeature.Destroy()
+            featureTest = v_layer.GetNextFeature()
+        gdal.RasterizeLayer(target_ds, [1], lyrBuffer)
+        target_ds = None
+        ds = None
+
+    elif geom.GetGeometryType() == ogr.wkbPolygon:
+        lyrBuffer = v_layer
+        if args.label_atr:
+            label_atr = ["ATTRIBUTE=" + args.label_atr]
+            # gdal.RasterizeLayer(ds,bands,layer,burn_values, options = ["BURN_VALUE_FROM=Z"])
+            gdal.RasterizeLayer(target_ds, [1], lyrBuffer, options=label_atr)
+            target_ds = None
+        else:
+            gdal.RasterizeLayer(target_ds, [1], lyrBuffer)
+            target_ds = None
+
+    else:
+        print("The Geometry type is neither Polygon nor Line")
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--raster", type=str, help="Raster file name  with directory")
+    parser.add_argument("--vector", type=str, help="Vector file name  with directory ")
+    parser.add_argument("--buffer", type=int, help="Buffer width of line feature", default=1)
+    parser.add_argument("--output_file", type=str, help="Output image name with directory")
+    parser.add_argument("--buffer_atr", type=str, help="Attribute from the vector file, This attribute can be"
+                                                       " buffer Width or It multiplies with buffer", default=None)
+    parser.add_argument("--label_atr", type=str, help="Attribute from the vector file to assign label to pixel."
+                                                      "Not required for single class")
+    args = parser.parse_args()
+    rasterize(args)
